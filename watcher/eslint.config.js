46c8027ef51733446082b60b05e46d686cc93ae4
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2025-04-17 16:36:59 +0200

    chore: migrate to eslint 9

---
+++ watcher/eslint.config.js
@@ -0,0 +1,66 @@
+import importPlugin from 'eslint-plugin-import';
+import sonarjsPlugin from 'eslint-plugin-sonarjs';
+import prettierConfig from 'eslint-config-prettier/flat';
+import globals from 'globals';
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  importPlugin.flatConfigs.recommended,
+  sonarjsPlugin.configs.recommended,
+  prettierConfig,
+  {
+    // default config
+    files: ['src/**/*', 'test/**/*'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+    rules: {
+      'default-param-last': 'off',
+      'new-cap': 'off',
+      'no-console': 'error',
+      'no-underscore-dangle': 'off',
+      'no-template-curly-in-string': 'off',
+      'max-classes-per-file': 'off',
+      'max-len': 'off',
+      'import/prefer-default-export': 'off',
+      'import/extensions': ['error', 'ignorePackages'],
+      'sonarjs/cognitive-complexity': ['warn', 15],
+      'sonarjs/no-duplicate-string': 'off',
+      'sonarjs/todo-tag': 'warn',
+    },
+    settings: {
+      'import/ignore': ['ethers'],
+    },
+  },
+  {
+    // test specific files using jest globals
+    files: ['test/**/*'],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-console': 'off',
+      'no-await-in-loop': 'off',
+      'sonarjs/no-clear-text-protocols': 'off',
+      'sonarjs/pseudo-random': 'off',
+    },
+  },
+  {
+    files: ['*.cjs'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      ecmaVersion: 2022,
+      sourceType: 'script',
+    },
+  },
+];
