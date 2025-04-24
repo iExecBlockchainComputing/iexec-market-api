b712eada177f80e9541021e00b1b98ac623143e5
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2025-04-24 09:47:19 +0200

    chore: stop indexing generated files

--- api/src/generated/api/package.js
+++
@@ -1,6 +0,0 @@
-// this file is auto generated do not edit it
-/* eslint-disable */
-export const name = "iexec-market-api";
-export const version = "6.4.1";
-export const description = "iexec marketplace api gateway";
-export default { name, version, description };
